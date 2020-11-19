d49740c325aab581fbd81815d4d9fcf0e876f3e8
Kalyan Vedula <kalyan.vedula@verser.com.au>
2020-11-19 11:12:33 +1100

    quantity issue fix

--- ConsoleApp1/ServiceHelpers/ShopifyAPIServices.cs
+++ ConsoleApp1/ServiceHelpers/ShopifyAPIServices.cs
@@ -62,37 +62,52 @@ namespace ConsoleApp1.ServiceHelpers
 
             foreach (dynamic order in DynamicData["orders"])
             {
-                var theModel = new OrderViewModel();                
-                string s = order["id"];
-                theModel.TIABOrderID = s;
-                var add = order["shipping_address"];
-                var order_lineItems = order["line_items"];  
-                theModel.FirstName = add["first_name"];
-                theModel.Surname = add["last_name"];
-                theModel.AddressLine1 = add["address1"];
-                theModel.Locality = add["city"];
-                theModel.SKU = order_lineItems[0]["sku"];
-                theModel.State = add["province_code"];
-                theModel.Postcode = add["zip"];
-                theModel.Shopify_OrderNumber = order["order_number"];
-                _Shopify_OrderNumber= order["order_number"];
-                string ContactNo = add["phone"];
-                strOrder_quantity =  order_lineItems[0]["quantity"];
-                if (!string.IsNullOrEmpty(strOrder_quantity))
-                {                  
-                    if (Convert.ToInt32(strOrder_quantity) >1)
-                    {
-                        Order_quantity = Convert.ToInt32(strOrder_quantity);
-                    } 
-                }
-                if (!string.IsNullOrEmpty(ContactNo))
+                //var theModel = new OrderViewModel();                
+                //string s = order["id"];
+                //theModel.TIABOrderID = s;
+                //var add = order["shipping_address"];
+                //var order_lineItems = order["line_items"][0];  
+                //theModel.FirstName = add["first_name"];
+                //theModel.Surname = add["last_name"];
+                //theModel.AddressLine1 = add["address1"];
+                //theModel.Locality = add["city"];
+                //theModel.SKU = order_lineItems["sku"];
+                //theModel.State = add["province_code"];
+                //theModel.Postcode = add["zip"];
+                //theModel.Shopify_OrderNumber = order["order_number"];
+                //_Shopify_OrderNumber= order["order_number"];
+                //string ContactNo = add["phone"];
+                //strOrder_quantity =  order_lineItems["quantity"];
+                //if (!string.IsNullOrEmpty(strOrder_quantity))
+                //{                  
+                //    if (Convert.ToInt32(strOrder_quantity) >1)
+                //    {
+                //        Order_quantity = Convert.ToInt32(strOrder_quantity);
+                //    } 
+                //}
+                //if (!string.IsNullOrEmpty(ContactNo))
+                //{
+                //    theModel.ContactNumber = Convert.ToInt32(ContactNo.Replace(" ", string.Empty).Replace("+61", string.Empty).Trim());
+                //}
+                //theModel.OrderType = "PhoneOnly";
+                //theModel.OrderSource = "ShopifyPortal";              
+                //theModel.OrderNumber = order["order_number"];
+                if (order["line_items"].Count > 0)
                 {
-                    theModel.ContactNumber = Convert.ToInt32(ContactNo.Replace(" ", string.Empty).Replace("+61", string.Empty).Trim());
+                    foreach (var item in order["line_items"])
+                    {
+                        if (item["quantity"] >= 1)
+                        {
+                            int quantity = item["quantity"];
+                            for (int i = 1; i <= quantity; i++)
+                            {
+                                modelCollection.Add(FillModel(order));
+                            }
+                        }
+                    }
                 }
-                theModel.OrderType = "PhoneOnly";
-                theModel.OrderSource = "ShopifyPortal";              
-                theModel.OrderNumber = order["order_number"];
-                modelCollection.Add(theModel);
+              
+                //modelCollection.Add(FillModel(order));
             }
 
             if (_Shopify_OrderNumber == "1600")
@@ -124,6 +139,42 @@ namespace ConsoleApp1.ServiceHelpers
                 }
             }
            
+        }
+
+        private static OrderViewModel FillModel(dynamic order)
+        {
+            var theModel = new OrderViewModel();
+            string s = order["id"];
+            theModel.TIABOrderID = s;
+            var add = order["shipping_address"];
+            var order_lineItems = order["line_items"][0];
+            theModel.FirstName = add["first_name"];
+            theModel.Surname = add["last_name"];
+            theModel.AddressLine1 = add["address1"];
+            theModel.Locality = add["city"];
+            theModel.SKU = order_lineItems["sku"];
+            theModel.State = add["province_code"];
+            theModel.Postcode = add["zip"];
+            theModel.Shopify_OrderNumber = order["order_number"];
+            //_Shopify_OrderNumber = order["order_number"];
+            string ContactNo = add["phone"];
+           // strOrder_quantity = order_lineItems["quantity"];
+            //if (!string.IsNullOrEmpty(strOrder_quantity))
+            //{
+            //    if (Convert.ToInt32(strOrder_quantity) > 1)
+            //    {
+            //        Order_quantity = Convert.ToInt32(strOrder_quantity);
+            //    }
+            //}
+            if (!string.IsNullOrEmpty(ContactNo))
+            {
+                theModel.ContactNumber = Convert.ToInt32(ContactNo.Replace(" ", string.Empty).Replace("+61", string.Empty).Trim());
+            }
+            theModel.OrderType = "PhoneOnly";
+            theModel.OrderSource = "ShopifyPortal";
+            theModel.OrderNumber = order["order_number"];
+            return theModel;
+
         }
     }
 }
